0a53d737b9721a8435f3d38d0066503ee084aa3d
Greice Fanka <greicefcardoso@gmail.com>
2020-09-28 20:35:15 -0300

    Adicionado deletar notas e buscar notas

--- app/models/note.js
+++ app/models/note.js
@@ -13,5 +13,6 @@ const noteSchema = new mongoose.Schema({
         required: true
       }
   });
+  noteSchema.index({'title': 'text', 'body': 'text'});
 
   module.exports = mongoose.model('Note', noteSchema);
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -16,6 +16,16 @@ router.post('/', withAuth, async (req, res) => {
 
 })
 
+router.get('/search', withAuth, async (req, res) => {
+    const { query } = req.query;
+    try {
+        let notes = await Note.find({author: req.user._id }).find({$text: {$search: query}});
+        res.json(notes);
+    } catch (error) {
+      res.json({error: error}).status(500);
+    }
+});
+
 router.get('/:id', withAuth, async (req, res) => {
     try {
         const { id } = req.params;
@@ -55,6 +65,20 @@ router.put('/:id', withAuth, async (req, res) => {
         res.status(500).json({ error: 'Problem to update a note' });
     }
 })
+
+router.delete('/:id', withAuth, async (req, res) => {
+    const {id} = req.params;
+    try {
+        let note = await Note.findById(id);
+        if(isOwner(req.user,note)){
+            await note.delete();
+            res.json({message: 'Ok'}).status(204);
+        } else 
+        res.status(403).json({ error: 'Permission denied' });
+    } catch (error) {
+        res.status(500).json({error: 'Problem to delete a note'}); 
+    }
+});
 
 
 const isOwner = (user, note) => {
